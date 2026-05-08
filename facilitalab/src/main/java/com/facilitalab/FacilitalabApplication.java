89eb12e2545607b43627deec7abc471ed8e67537
JoãoMNS <joaomnsilvadev@gmail.com>
2026-05-07 21:01:19 -0300

    Add files via upload

---
+++ facilitalab/src/main/java/com/facilitalab/FacilitalabApplication.java
@@ -0,0 +1,13 @@
+package com.facilitalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitalabApplication.class, args);
+	}
+
+}
