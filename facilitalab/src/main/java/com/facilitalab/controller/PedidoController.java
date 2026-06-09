0f24fc2aa88b463b28c711c7023030dbf0a497a1
JoãoMNS <joao.matheus.n.silva@gmail.com>
2026-06-09 15:26:13 -0300

    feat(tarefa-8): implementa isolamento de dados por usuário autenticado no PedidoService

--- facilitalab/src/main/java/com/facilitalab/controller/PedidoController.java
+++ facilitalab/src/main/java/com/facilitalab/controller/PedidoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.facilitalab.dtos.PedidoCreateDTO;
 import com.facilitalab.dtos.PedidoSaidaDTO;
+import com.facilitalab.exception.AcessoNegadoException;
 import com.facilitalab.exception.TransicaoInvalidaException;
 import com.facilitalab.models.EstadoEnum;
 import com.facilitalab.models.PrioridadeEnum;
@@ -29,8 +30,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
 @RestController
 @RequestMapping("/pedidos")
 @RequiredArgsConstructor
@@ -38,10 +37,9 @@ public class PedidoController {
 
     private final PedidoService pedidoService;
 
-
     // POST /pedidos
     @PostMapping
-    public  ResponseEntity<PedidoSaidaDTO> criarPedido(@Valid @RequestBody PedidoCreateDTO dto) {
+    public ResponseEntity<PedidoSaidaDTO> criarPedido(@Valid @RequestBody PedidoCreateDTO dto) {
         return ResponseEntity.status(201).body(pedidoService.criarPedido(dto));
     }
 
@@ -85,16 +83,17 @@ public class PedidoController {
     // UPDATE /{id}/estado
     @PutMapping("/{id}/estado")
     public ResponseEntity<PedidoSaidaDTO> atualizarEstado(
-    @PathVariable Long id,
-    @RequestParam EstadoEnum novoEstado,
-    @RequestParam(required = false) Long cadistaId) {
-    return ResponseEntity.ok(pedidoService.atualizarEstadoPedido(id, novoEstado, cadistaId));
+            @PathVariable Long id,
+            @RequestParam EstadoEnum novoEstado,
+            @RequestParam(required = false) Long cadistaId) {
+        return ResponseEntity.ok(pedidoService.atualizarEstadoPedido(id, novoEstado, cadistaId));
     }
 
     // UPDATE
     @PutMapping("/{id}")
-    public ResponseEntity<PedidoSaidaDTO> atualizarPedido(@PathVariable Long id, @Valid @RequestBody PedidoCreateDTO dto) {
-    return ResponseEntity.ok(pedidoService.atualizarPedido(id, dto));
+    public ResponseEntity<PedidoSaidaDTO> atualizarPedido(@PathVariable Long id,
+            @Valid @RequestBody PedidoCreateDTO dto) {
+        return ResponseEntity.ok(pedidoService.atualizarPedido(id, dto));
     }
 
     // DELETE
@@ -127,7 +126,10 @@ public class PedidoController {
                 .body(Map.of("errors", List.of(ex.getMessage())));
     }
 
-
-
+    @ExceptionHandler(AcessoNegadoException.class)
+    public ResponseEntity<Map<String, List<String>>> handleAcessoNegado(AcessoNegadoException ex) {
+        return ResponseEntity.status(403)
+                .body(Map.of("errors", List.of(ex.getMessage())));
+    }
 
 }
